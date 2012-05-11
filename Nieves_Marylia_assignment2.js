0d294945faea1d0a3a886c077da0972eaf10a4c3
marylian <marylian@fullsail.edu>
2012-05-10 22:06:26 -0300

    Added the boolean function.

--- Nieves_Marylia_assignment2.js
+++ Nieves_Marylia_assignment2.js
@@ -1,5 +1,8 @@
 var myBudget; //Declaration of numeric variable
 var procedureFunc;
+var vehicleMake; //Declaration of string variable
+var gasMileage; //Declaration of numeric variable
+
 
 procedureFunc=function(myBudget){ //Procedure without any return.
 if (myBudget<25000){ 
@@ -10,7 +13,25 @@ else {
     }
 };
 
+booleanFunc=function(vehicleMake, gasMileage){ //Boolean function
+ console.log("Which car is this and how much is the mileage?");
+if (vehicleMake=="Hyundai" && gasMileage>="40"){ //Comparision operation
+    console.log("This is a "+vehicleMake+" and it gives "+gasMileage+" MPG"); //Output if condition is met.
+    return (booleanValue=true); //Boolean returned back.
+}
+     else{
+         console.log("I would like to see a "+vehicleMake+" first.") //Output if condition is not met.
+         return (booleanValue=false); // Boolean returned back.
+     }
+};
 
 
 
 procedureFunc(20000); //Calling the procedure with one argument.
+booleanFunc("Hyundai", 39); //Calling boolean function with 2 arguments.
+if (booleanValue==true){
+    console.log("I cant believe they make such good cars");
+}
+else{
+    console.log("I would still like to see a Hyundai");
+} //End boolean function.
